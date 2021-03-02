f828ac9fcb5f15c789e507204f2a74c0601a05fb
azranar-66 <74614652+azranar-66@users.noreply.github.com>
2021-03-02 20:08:07 +0300

    add

--- add.php
+++ add.php
@@ -1,11 +1,24 @@
 <?php
+session_start();
 include "function.php";
+if (!empty($_POST["name"])) {
+    if (!empty($_POST["msg"])) {
 
-$badWords = checkCensor($_POST["msg"]);
+        $badWords = checkCensor($_POST["msg"]);
 
-if (empty($badWords)) {
-    saveMessage($_POST["msg"], $_POST["name"]);
+        if (empty($badWords)) {
+            if (!isset($_SESSION["ban_time"])) {
+                saveMessage($_POST["msg"], $_POST["name"]);
+            }
+        } else {
+            $_SESSION["ban_time"] = time();
+            saveBadWordsToLog($badWords);
+        }
+    } else {
+        $_SESSION["empty_msg"] = "Сообщение не может быть пустым!";
+    }
 } else {
-    saveBadWordsToLog($badWords);
+    $_SESSION["empty_name"] = "Имя не может быть пустым";
 }
+
 header("Location: main.php");
